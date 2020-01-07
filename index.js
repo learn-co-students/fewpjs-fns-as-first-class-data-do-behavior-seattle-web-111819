734b9b22de4892429344a837ac82c69149f72388
Tanner Patterson <tpatterson5492@gmail.com>
2020-01-07 11:28:12 -0800

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string
+}
+
+
+
+function greet(string) {
+  let nString = parseInt(string, 10);
+  if (nString < 12) return `Good Morning`;
+  if (nString > 17) return `Good Evening`;
+  return `Good Afternoon`
+  }
+
+  // function displayMessage()
