a6b56b0d03325d7b6c532c7c1df414814d0ec85d
NicoleLC16 <asalwaysnicole@outlook.com>
2021-04-12 17:21:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-/* Given Code, don't edit */
+document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-function greet(timeString) {
-    const time = parseInt(timeString.split(':'))
-    if (time < 12) {
-      return "Good Morning";
-    }
-    else if (time > 17 ) {
-      return  "Good Evening";
-    }
-    else {
-      return "Good Afternoon";
-    }
+function greet(str) {
+  console.log(str.split(":"));
+  const time = parseInt(str.split(":"));
 
+  if (time < 12) {
+    return "Good Morning";
+  } else if (time > 16) {
+    return "Good Evening";
+  } else {
+    return "Good Afternoon";
+  }
+}
+
+function displayMessage(str) {
+  document.querySelector("#greeting").innerText = str;
 }
 /* Write your implementation of displayMessage() */
-function displayMessage(greet) {
-  document.getElementById('greeting').innerText = greet;
-}
+
+// /* Given Code, don't edit */
+
+// function handleClick(e) {
+//   const timeString = document.getElementById('time').value
+//   displayMessage(greet(timeString))
+// }
+
+// /* End Given Code, don't edit above here...*/
+
+// /* Write your implementation of greet() */
+// function greet(timeString) {
+//     const time = parseInt(timeString.split(':'))
+//     if (time < 12) {
+//       return "Good Morning";
+//     }
+//     else if (time > 17 ) {
+//       return  "Good Evening";
+//     }
+//     else {
+//       return "Good Afternoon";
+//     }
+
+// }
+// /* Write your implementation of displayMessage() */
+// function displayMessage(greet) {
+//   document.getElementById('greeting').innerText = greet;
+// }
